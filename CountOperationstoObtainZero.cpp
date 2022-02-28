062222cb58f6bded189a8eec60f37352c993c464
Varun Sharma <9204varun@gmail.com>
2022-02-28 21:06:57 +0530

    Create CountOperationstoObtainZero.cpp

---
+++ CountOperationstoObtainZero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int count = 0;
+        if(num1 == 0 || num2 == 0){
+            return 0;
+        }
+        while(num1 != num2){
+            if(num1 > num2){
+                num1 = num1 - num2;
+            }else{
+                num2 = num2 - num1;
+            }
+            
+            count++;
+        }
+    count++;
+    return count;
+    }
+};
