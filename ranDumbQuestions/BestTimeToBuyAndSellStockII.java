e4ade13c77ed184be4bd0d991c7af2ee0eaac179
Varun Sharma <9204varun@gmail.com>
2024-03-21 22:57:29 +0530

    Create BestTimeToBuyAndSellStockII.java

---
+++ ranDumbQuestions/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        
+        int ans = 0;
+        for(int i=0; i<n-1; i++){
+            if(prices[i] < prices[i+1]){
+                ans += prices[i+1] - prices[i];
+            }
+        }
+        return ans;
+    }
+}
