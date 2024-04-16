90bf1f336456d89628973ee57d783b2793598bf8
Varun Sharma <9204varun@gmail.com>
2024-04-16 23:48:44 +0530

    Templates & Snippets

--- dailyChallenge/LengthOfLastWord.java
+++ dailyChallenge/LengthOfLastWord.java
@@ -1,19 +1,15 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int n = s.length();
-        int flag = 0;
         int count = 0;
-        for(int i = n-1; i>=0; i--){
-            if(s.charAt(i) != ' '){
-                flag = 1;
-            }
-            if(flag == 1){
-                if(s.charAt(i) == ' '){
-                    break;
-                }
+        int ans = 0;
+        for(int i=0; i<s.length(); i++){
+            if(' ' != s.charAt(i)){
                 count++;
+            }else{
+                ans = count == 0 ? ans : count;
+                count = 0;
             }
         }
-        return count;
+        return count == 0 ? ans : count;
     }
 }
