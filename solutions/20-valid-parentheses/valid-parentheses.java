125ba0b357cba25c27a0298ce28299900c9cdaa7
Varun Sharma <9204varun@gmail.com>
2024-04-18 23:48:25 +0530

    Time: 1 ms (98.61%) | Memory: 41.2 MB (86.39%) - LeetSync

--- solutions/20-valid-parentheses/valid-parentheses.java
+++ solutions/20-valid-parentheses/valid-parentheses.java
@@ -1,34 +1,17 @@
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> st = new Stack();
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
-                st.push(s.charAt(i));
-            }else{
-                if(st.size() <= 0){
-                    return false;
-                }
-                char a = st.peek();
-                st.pop();
-                if(s.charAt(i) == ')'){
-                    if(a != '('){
-                       return false;
-                    }
-                }
-                if(s.charAt(i) == ']'){
-                    if(a != '['){
-                       return false;
-                    }
-                }if(s.charAt(i) == '}'){
-                    if(a != '{'){
-                       return false;
-                    }
-                }
-            }
-        }
-        if(st.size() > 0){
-            return false;
-        }
-        return true;
-    }
+  public boolean isValid(String s) {
+    Deque<Character> stack = new ArrayDeque<>();
+
+    for (final char c : s.toCharArray())
+      if (c == '(')
+        stack.push(')');
+      else if (c == '{')
+        stack.push('}');
+      else if (c == '[')
+        stack.push(']');
+      else if (stack.isEmpty() || stack.pop() != c)
+        return false;
+
+    return stack.isEmpty();
+  }
 }
