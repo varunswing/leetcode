bccd0882996ba351c6590557b1447ca932bcd1d3
Varun Sharma <9204varun@gmail.com>
2024-04-19 20:12:30 +0530

    Time: 1 ms (96.18%) | Memory: 44.6 MB (94.78%) - LeetSync

--- solutions/2058-concatenation-of-array/concatenation-of-array.java
+++ solutions/2058-concatenation-of-array/concatenation-of-array.java
@@ -4,10 +4,7 @@ class Solution {
         int[] ans = new int[2*n];
         
         for(int i=0; i<n; i++){
-            ans[i] = nums[i];
-        }
-        for(int i=0; i<n; i++){
-            ans[n+i] = nums[i];
+            ans[i] = ans[n+i] = nums[i];
         }
         return ans;
     }
