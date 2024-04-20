566b561046b486626bec148f3ee8d54c972cb63f
Varun Sharma <9204varun@gmail.com>
2024-04-20 23:00:17 +0530

    Time: 4 ms (99.66%) | Memory: 45.3 MB (53.77%) - LeetSync

--- solutions/680-valid-palindrome-ii/valid-palindrome-ii.java
+++ solutions/680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,31 +1,15 @@
 class Solution {
-    
-    public static boolean isPalindrome(String str) {
-        String cleanStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        return cleanStr.equals(new StringBuilder(cleanStr).reverse().toString());
-    }
-    
-    public boolean validPalindrome(String s) {
-        int n = s.length();
-        if(isPalindrome(s)){
-            return true;
-        }
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) != s.charAt(n-i-1)){
-                String so = s;
-                String st = s;
-                String os = so.substring(0, i) + so.substring(i + 1);
-                
-                String ts = st.substring(0, n-1-i) + st.substring(n-1-i + 1);
-                
-                if(isPalindrome(os) || isPalindrome(ts)){
-                    System.out.println("b");
-                    return true;
-                }
-                return false;
-            }
-        }
-        System.out.println("c");
-        return true;
-    }
+  public boolean validPalindrome(String s) {
+    for (int l = 0, r = s.length() - 1; l < r; ++l, --r)
+      if (s.charAt(l) != s.charAt(r))
+        return validPalindrome(s, l + 1, r) || validPalindrome(s, l, r - 1);
+    return true;
+  }
+
+  private boolean validPalindrome(final String s, int l, int r) {
+    while (l < r)
+      if (s.charAt(l++) != s.charAt(r--))
+        return false;
+    return true;
+  }
 }
