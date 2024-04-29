617da83f4d6f72736bdec04f78cddbecc6e3aadd
Varun Sharma <9204varun@gmail.com>
2024-04-30 05:27:04 +0530

    Time: 1 ms (100.00%) | Memory: 56.4 MB (59.47%) - LeetSync

--- solutions/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ solutions/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -1,6 +1,16 @@
 class Solution {
-  public int minOperations(int[] nums, int k) {
-    final int xors = Arrays.stream(nums).reduce((a, b) -> a ^ b).getAsInt();
-    return Integer.bitCount(k ^ xors);
-  }
+    public int minOperations(int[] nums, int k) {
+        int xor = nums[0];
+        for(int i=1; i<nums.length; i++){
+            xor ^= nums[i];
+        }
+        
+        int ans = xor^k;
+        int count = 0;
+        while(ans > 0){
+            count += ans&1;
+            ans >>= 1;
+        }
+        return count;
+    }
 }
