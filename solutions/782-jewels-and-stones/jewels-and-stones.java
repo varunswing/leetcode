fb4c4ed23292820692de8a06badc83978659939a
Varun Sharma <9204varun@gmail.com>
2024-04-30 06:16:51 +0530

    Time: 4 ms (12.74%) | Memory: 42.1 MB (7.98%) - LeetSync

--- solutions/782-jewels-and-stones/jewels-and-stones.java
+++ solutions/782-jewels-and-stones/jewels-and-stones.java
@@ -1,14 +1,11 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        Map<Character, Integer> m = new HashMap();
-        for(int i=0; i<stones.length(); i++){
-            m.put(stones.charAt(i), m.getOrDefault(stones.charAt(i), 0) + 1);
-            // System.out.println(m.get(stones.charAt(i)));
-        }
-        
-        int ans = 0;
-        for(int i=0; i<jewels.length(); i++){
-            ans += m.getOrDefault(jewels.charAt(i), 0);
+        Set<Character> jewel = jewels.chars().mapToObj(c->(char) c).collect(Collectors.toSet());
+        int ans = 0;   
+        for(char arr: stones.toCharArray()){
+            if(jewel.contains(arr)){
+                ans++;
+            }
         }
         return ans;
     }
