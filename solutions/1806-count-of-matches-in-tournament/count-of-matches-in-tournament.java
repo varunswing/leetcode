3e11df81207ea13bd5f5161e495f0e2235ac46aa
Varun Sharma <9204varun@gmail.com>
2024-05-15 22:44:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (57.37%) - LeetSync

--- solutions/1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ solutions/1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,16 +1,5 @@
 class Solution {
     public int numberOfMatches(int n) {
-        int ans = 0;
-        while(n > 1){
-            if(n%2 == 0){
-                n /=2;
-                ans += n;
-            }else{
-                n /= 2;
-                ans += n;
-                n++;
-            }
-        }
-        return ans;
+        return n-1;
     }
 }
