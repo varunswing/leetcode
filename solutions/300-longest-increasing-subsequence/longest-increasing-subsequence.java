5a92341b0cc4c90e4a09d5e6a06585c4e76988a1
Varun Sharma <9204varun@gmail.com>
2024-07-05 01:22:38 +0530

    Time: 43 ms (51.82%) | Memory: 44.4 MB (34.42%) - LeetSync

--- solutions/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ solutions/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -2,37 +2,25 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         
-        List<Integer> a = new ArrayList();
-        int ans = 0;
-        
+        int[] ans = new int[n];
         for(int i=0; i<n; i++){
-            int index = binarySearch(a, nums[i]);
-            
-            if(index == a.size()){
-                a.add(nums[i]);
-            }else{
-                a.set(index, nums[i]);
-            }
-            
+            ans[i] = 1;
         }
-
-        return a.size();
-    }
-    
-    private int binarySearch(List<Integer> sorted, int target) {
-        int left = 0, right = sorted.size();
-        int result = sorted.size();
+        int i = 1, j = 0;
+        while(i < n){
+            while(j < i){
+                if(nums[j] < nums[i]){
+                    ans[i] = Math.max(ans[i], 1 + ans[j]);
+                }
+                // ans[i] = Math.max(ans[i], ans[j]); 
+                j++;
+            }
+            j = 0;
+            i++;
+        }
         
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (sorted.get(mid) < target) {
-                left = mid + 1;
-            } else {
-                result = mid;
-                right = mid;
-            }
-        }
-        return result;
+        Arrays.sort(ans);
+        
+        return ans[n-1];
     }
 }
