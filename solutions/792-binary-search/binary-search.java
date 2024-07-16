d1731f2bf855d04cb99d33426a32d15986a85dd4
Varun Sharma <9204varun@gmail.com>
2024-07-16 21:23:19 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (35.30%) - LeetSync

---
+++ solutions/792-binary-search/binary-search.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return bs(0, nums.length-1, nums, target);
+    }
+    
+    public int bs(int l, int r, int[] nums, int k){
+        int m = l + (r-l)/2;
+        if(l < 0 || r > nums.length-1 || l>r){
+            return -1;
+        }
+        
+        if(nums[m] == k){
+            return m;
+        }
+
+        if(l==m && r==m){
+            return -1;
+        }
+
+        if(nums[m] > k){
+            r = m-1;
+        }else{
+            l = m+1;
+        }
+        
+        return bs(l, r, nums, k);
+    }
+}
