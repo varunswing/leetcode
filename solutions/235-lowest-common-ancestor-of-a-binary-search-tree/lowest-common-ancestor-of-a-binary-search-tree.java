f5da22c0a2dad4abc518bd03c9a9ea7eee04f2fa
Varun Sharma <9204varun@gmail.com>
2024-07-16 22:22:07 +0530

    Time: 5 ms (100.00%) | Memory: 44.9 MB (55.16%) - LeetSync

--- solutions/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ solutions/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -14,18 +14,13 @@ class Solution {
         if(root == null){
             return root;
         }
-        if(root.val == q.val || root.val == p.val){
-            return root;
-        }
-
-        if((root.val > p.val && root.val < q.val) || (root.val < p.val && root.val > q.val)){
-            return root;
-        }
 
         if(root.val > p.val && root.val > q.val){
             return lowestCommonAncestor(root.left, p, q);
-        }else{
+        }else if(root.val < p.val && root.val < q.val){
             return lowestCommonAncestor(root.right, p, q);
+        }else{
+            return root;
         }
     }
 }
