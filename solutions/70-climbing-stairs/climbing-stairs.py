d97e29d422024591d11cc309eb559e98fa030aed
Varun Sharma <9204varun@gmail.com>
2024-08-13 12:36:49 +0530

    Time: 27 ms (93.09%) | Memory: 16.5 MB (56.44%) - LeetSync

--- solutions/70-climbing-stairs/climbing-stairs.py
+++ solutions/70-climbing-stairs/climbing-stairs.py
@@ -1,7 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1: return 1
-        if n == 2: return 2
         prev1 = 1
         prev2 = 2
         for i in range(2, n):
@@ -9,5 +8,3 @@ class Solution:
             prev1 = prev2
             prev2 = cur
         return prev2
-
-        
