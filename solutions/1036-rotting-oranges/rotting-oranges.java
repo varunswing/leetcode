ab07cd557552089c6d28c57e0cfa35830a456be0
Varun Sharma <9204varun@gmail.com>
2024-09-02 21:07:01 +0530

    Time: 2 ms (71.40%) | Memory: 42.1 MB (77.32%) - LeetSync

--- solutions/1036-rotting-oranges/rotting-oranges.java
+++ solutions/1036-rotting-oranges/rotting-oranges.java
@@ -1,46 +1,45 @@
+import java.util.*;
+
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        Queue<int[]> rotten=new LinkedList<>();
-        int freshCount =0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    rotten.offer(new int[]{i,j});
-                }
-                if(grid[i][j]==1)freshCount++;
-            }
-        }
-        int rottenTime=0;
-        int madeRotten=0;
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int m = grid.length;
+        int n = grid[0].length;
+        int ans = 0;
+        int countFresh = 0;
+        Queue<int[]> q = new LinkedList<>();
 
-        while(!rotten.isEmpty()&& freshCount>0){
-            rottenTime++;
-            int queueSize = rotten.size();
-            for(int i=0;i<queueSize;i++){
-                int[] curr = rotten.poll();
-                
-                int x=curr[0];
-                int y = curr[1];
-                
-                
-                    
-                    for(int j=0;j<4;j++){
-                        int nx=x+dir[j][0];
-                        int ny=y+dir[j][1];
-                        if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==1){
-                            grid[nx][ny]=2;
-                            rotten.offer(new int[]{nx,ny});
-                            freshCount--;
-                        }
-                        
-                    }
-                
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (grid[i][j] == 1)
+                    ++countFresh;
+                else if (grid[i][j] == 2)
+                    q.offer(new int[] {i, j});
+
+        if (countFresh == 0)
+            return 0;
+
+        while (!q.isEmpty()) {
+            ++ans;
+            int sz = q.size();
+            for (int i = 0; i < sz; ++i) {
+                int[] cell = q.poll();
+                int x = cell[0];
+                int y = cell[1];
+                for (int[] dir : dirs) {
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+                    if (nx < 0 || nx == m || ny < 0 || ny == n)
+                        continue;
+                    if (grid[nx][ny] != 1)
+                        continue;
+                    grid[nx][ny] = 2;   // Mark grid[nx][ny] as rotten.
+                    q.offer(new int[] {nx, ny});  // Push the newly rotten orange to the queue.
+                    --countFresh;     // Decrease the count of fresh oranges by 1.
+                }
             }
-        
         }
-        return freshCount==0 ? rottenTime:-1 ;
+
+        return countFresh == 0 ? ans - 1 : -1;
     }
 }
