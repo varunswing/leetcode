4a0d2c9e6bfcb92eead9ea98b20057c6faf9e631
Varun Sharma <9204varun@gmail.com>
2024-09-02 22:44:08 +0530

    Time: 43 ms (26.36%) | Memory: 16.7 MB (15.06%) - LeetSync

--- solutions/3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.py
+++ solutions/3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.py
@@ -3,13 +3,12 @@ class Solution:
         arr = [int(ch) for ch in s]
         n = len(arr)
 
-        for i in range(n):
-            for j in range(1, n):
-                if arr[j-1] % 2 == arr[j] % 2:
-                    if arr[j-1] > arr[j]:
-                        arr[j-1] = arr[j] + arr[j-1]
-                        arr[j] = arr[j-1] - arr[j]
-                        arr[j-1] = arr[j-1] - arr[j]
-        
-                        return "".join([str(num) for num in arr])
+        for j in range(1, n):
+            if arr[j-1] % 2 == arr[j] % 2:
+                if arr[j-1] > arr[j]:
+                    arr[j-1] = arr[j] + arr[j-1]
+                    arr[j] = arr[j-1] - arr[j]
+                    arr[j-1] = arr[j-1] - arr[j]
+    
+                    return "".join([str(num) for num in arr])
         return "".join([str(num) for num in arr])
