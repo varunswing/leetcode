074a5691c6042e1c682895c69746a992be6e42d1
Varun Sharma <9204varun@gmail.com>
2024-10-15 00:31:27 +0530

    Time: 18 ms (44.53%) | Memory: 45.7 MB (24.01%) - LeetSync

--- solutions/1091-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ solutions/1091-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,34 +1,38 @@
 class Solution {
-  public int shortestPathBinaryMatrix(int[][] grid) {
-    final int n = grid.length;
-    if (grid[0][0] == 0 && n == 1)
-      return 1;
-    if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
-      return -1;
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        if (grid[0][0] == 0 && n == 1)
+            return 1;
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+            return -1;
 
-    final int[][] dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-    Queue<Pair<Integer, Integer>> q = new ArrayDeque<>(List.of(new Pair<>(0, 0)));
-    boolean[][] seen = new boolean[n][n];
-    seen[0][0] = true;
+        boolean[][] seen = new boolean[n][n];
+        seen[0][0] = true;
 
-    for (int step = 1; !q.isEmpty(); ++step)
-      for (int sz = q.size(); sz > 0; --sz) {
-        final int i = q.peek().getKey();
-        final int j = q.poll().getValue();
-        for (int[] dir : dirs) {
-          final int x = i + dir[0];
-          final int y = j + dir[1];
-          if (x < 0 || x == n || y < 0 || y == n)
-            continue;
-          if (grid[x][y] != 0 || seen[x][y])
-            continue;
-          if (x == n - 1 && y == n - 1)
-            return step + 1;
-          q.offer(new Pair<>(x, y));
-          seen[x][y] = true;
+        int[][] dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        q.offer(new Pair(0, 0));
+
+        for(int i=1; q.size()>0; i++){
+            for(int j=q.size()-1; j>=0; j--){
+                int xi = q.peek().getKey();
+                int yi = q.poll().getValue();
+                for (int[] dir : dirs) {
+                    final int x = xi + dir[0];
+                    final int y = yi + dir[1];
+                    if (x < 0 || x == n || y < 0 || y == n)
+                        continue;
+                    if (grid[x][y] != 0 || seen[x][y])
+                        continue;
+                    if (x == n - 1 && y == n - 1)
+                        return i + 1;
+                    q.offer(new Pair<>(x, y));
+                    seen[x][y] = true;
+                }
+            }
         }
-      }
 
-    return -1;
-  }
+        return -1;
+    }
 }
