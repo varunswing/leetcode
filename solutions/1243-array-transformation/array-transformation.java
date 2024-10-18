7a9995bd17d8fef9ee97418402a9d75b77cf91bd
Varun Sharma <9204varun@gmail.com>
2024-10-18 23:38:15 +0530

    Time: 1 ms (100.00%) | Memory: 41.6 MB (41.30%) - LeetSync

--- solutions/1243-array-transformation/array-transformation.java
+++ solutions/1243-array-transformation/array-transformation.java
@@ -1,19 +1,26 @@
 class Solution {
-  public List<Integer> transformArray(int[] arr) {
-    if (arr.length < 3)
-      return Arrays.stream(arr).boxed().toList();
-
-    int[] ans = new int[0];
-
-    while (!Arrays.equals(ans, arr)) {
-      ans = arr.clone();
-      for (int i = 1; i < arr.length - 1; ++i)
-        if (ans[i - 1] > ans[i] && ans[i] < ans[i + 1])
-          ++arr[i];
-        else if (ans[i - 1] < ans[i] && ans[i] > ans[i + 1])
-          --arr[i];
+    public List<Integer> transformArray(int[] arr) {
+        int cnt = 1;
+        int n = arr.length;
+        while(cnt != 0){
+            cnt = 0;
+            int[] na = arr.clone();
+            for(int i=1; i<arr.length-1; i++){
+                if(na[i] > na[i-1] && na[i] > na[i+1]){
+                    cnt++;
+                    arr[i]--;
+                }
+                if(na[i] < na[i-1] && na[i] < na[i+1]){
+                    cnt++;
+                    arr[i]++;
+                }
+            }
+        }
+        ArrayList<Integer> newList = new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            newList.add(arr[i]);
+        }
+        return newList;
+        
     }
-
-    return Arrays.stream(ans).boxed().toList();
-  }
 }
