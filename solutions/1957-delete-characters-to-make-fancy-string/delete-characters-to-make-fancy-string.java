5e90231787ca94eee238d806531a858cc3a91883
Varun Sharma <9204varun@gmail.com>
2024-10-22 18:16:57 +0530

    Time: 46 ms (30.97%) | Memory: 45.4 MB (79.96%) - LeetSync

--- solutions/1957-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ solutions/1957-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -1,20 +1,10 @@
 class Solution {
-    public String makeFancyString(String s) {
-        int count = 0;
-        StringBuilder sb = new StringBuilder(s);
-        for(int i=1; i<sb.length(); i++){
-            if(sb.charAt(i) == sb.charAt(i-1)){
-                count++;
-            }else{
-                count = 0;
-            }
-            if(count == 2){
-                sb.deleteCharAt(i);
-                count--;
-                i--;
-            }
-        }
-        return sb.toString();
-        
-    }
+  public String makeFancyString(String s) {
+    StringBuilder sb = new StringBuilder();
+    for (char c : s.toCharArray())
+      if (sb.length() < 2 || 
+          sb.charAt(sb.length() - 1) != c || sb.charAt(sb.length() - 2) != c)
+        sb.append(c);
+    return sb.toString();
+  }
 }
