eb22975f5c7ea8748016691ff7a3542f692996b0
Varun Sharma <9204varun@gmail.com>
2024-10-23 20:27:41 +0530

    Time: 41 ms (96.13%) | Memory: 112.6 MB (80.93%) - LeetSync

--- solutions/146-lru-cache/lru-cache.java
+++ solutions/146-lru-cache/lru-cache.java
@@ -1,51 +1,77 @@
-class Node{
-    int key;
-    int value;
-    public Node(int key, int value){
-        this.key = key;
-        this.value = value;
-    }
-}
-
-class LRUCache {
+class LRUCache
+{
+    private class Node{
+        int key;
+        int val;
+        Node prev, next;
+        
+        Node(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
     
-
-    public LRUCache(int capacity) {
+    }
+    
+    public LRUCache(int capacity){
         this.capacity = capacity;
+        this.cache = new HashMap();
+        
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
     }
     
-    public Map<Integer, Node> m = new HashMap();
-    public Set<Node> cache = new LinkedHashSet();
-    public int capacity;
+    private HashMap<Integer, Node> cache;
     
+    private Node head;
     
-    public int get(int key) {
-        if(m.containsKey(key)){
-            Node node = m.get(key);
-            cache.remove(node);
-            cache.add(node);
-            return node.value;
+    private Node tail;
+    
+    private int capacity;
+    
+    public int get(int key){
+        if(!cache.containsKey(key)){
+            return -1;
         }
-        return -1;
+        Node node = cache.get(key);
+        
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        
+        insert(node);
+        return node.val;
     }
     
-    public void put(int key, int value) {
-        if(m.containsKey(key)){
-            m.get(key).value = value;
-            get(key);
-            return;
-        }
-        if (cache.size() == capacity) {
-          Node lastNode = cache.iterator().next();
-          cache.remove(lastNode);
-          m.remove(lastNode.key);
-        }
-
-        Node node = new Node(key, value);
-        cache.add(node);
-        m.put(key, node);
+    public void put(int key, int val){
+        if(cache.containsKey(key)){
+            Node node = cache.get(key);
+            node.val = val;
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            
+            insert(node);
+        }else{
+            if(cache.size() == capacity){
+                cache.remove(tail.prev.key); //LRU
+                
+                tail.prev.prev.next = tail.prev.next;
+                tail.prev.next.prev = tail.prev.prev;
+            }
+            
+            Node n = new Node(key, val);
+            cache.put(key, n);
+            insert(n);
         
-        
+            
+        }
+    }
+    
+    private void insert(Node node){
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
     }
 }
 
