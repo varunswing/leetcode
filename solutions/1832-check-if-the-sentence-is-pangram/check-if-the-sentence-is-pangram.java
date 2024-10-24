0f1d94402b65915504fc4fbff8c5a548a1332e73
Varun Sharma <9204varun@gmail.com>
2024-10-24 19:08:04 +0530

    Time: 3 ms (50.92%) | Memory: 41.7 MB (19.84%) - LeetSync

--- solutions/1832-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ solutions/1832-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,21 +1,10 @@
 class Solution {
-    public boolean checkIfPangram(String sentence) {
-        int[] all = new int[26];
-        int n = sentence.length();
-        if(n<26){
-            return false;
-        }
+  public boolean checkIfPangram(String sentence) {
+    Set<Character> seen = new HashSet<>();
 
-        for(char c : sentence.toCharArray()){
-            all[c-'a']++;
-        }
+    for (final char c : sentence.toCharArray())
+      seen.add(c);
 
-        for(int i=0; i<26; i++){
-            if(all[i] <= 0){
-                return false;
-            }
-        }
-
-        return true;
-    }
+    return seen.size() == 26;
+  }
 }
