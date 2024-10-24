038f11447bed425003045a75c32d82cc8125d578
Varun Sharma <9204varun@gmail.com>
2024-10-24 23:10:05 +0530

    Time: 183 ms (5.18%) | Memory: 45.5 MB (6.53%) - LeetSync

--- solutions/1356-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ solutions/1356-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,11 +1,32 @@
 class Solution {
-  public int[] sortByBits(int[] arr) {
-    Integer[] A = Arrays.stream(arr).boxed().toArray(Integer[] ::new);
-    Arrays.sort(A,
-                (a, b)
-                    -> Integer.bitCount(a) == Integer.bitCount(b)
-                           ? a - b
-                           : Integer.bitCount(a) - Integer.bitCount(b));
-    return Arrays.stream(A).mapToInt(Integer::intValue).toArray();
-  }
+  
+    public static void swap(int i,int j,int arr[]){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        return;
+    }
+    public static int[] sort(HashMap<Integer,Integer> map,int arr[]){
+        int size=arr.length;
+        // lets implement bubble sort in here
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(map.get(arr[i])>map.get(arr[j])) swap(i,j,arr);
+                else if(map.get(arr[i])==map.get(arr[j])){
+                    if(arr[i]>arr[j]) swap(i,j,arr);
+                }
+            }
+        }
+        return arr;
+    }
+    public int[] sortByBits(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int size=arr.length;
+        for(int i=0;i<size;i++){
+            int bits=Integer.bitCount(arr[i]);
+            map.put(arr[i],bits);
+        }
+       // System.out.println(map);
+        return sort(map,arr);
+    }
 }
