adfe025eccf6772b7b67e6c327ccd2322dd593bc
Varun Sharma <9204varun@gmail.com>
2024-10-26 01:04:18 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (86.60%) - LeetSync

--- solutions/3325-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.java
+++ solutions/3325-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.java
@@ -1,20 +1,17 @@
-public class Solution {
-    public int numberOfSubstrings(String s, int k) {
-        int ans = 0;
+class Solution {
+  public int numberOfSubstrings(String s, int k) {
+    final int n = s.length();
+    int ans = n * (n + 1) / 2;
+    int[] count = new int[26];
 
-        for (int i = 0; i < s.length(); i++) {
-            int[] freqArr = new int[26];
-
-            for (int j = i; j < s.length(); j++) {
-                freqArr[s.charAt(j) - 'a']++;
-
-                if (freqArr[s.charAt(j) - 'a'] == k) {
-                    ans += s.length() - j;
-                    break;
-                }
-            }
-        }
-
-        return ans;
+    for (int l = 0, r = 0; r < n; ++r) {
+      final int c = s.charAt(r) - 'a';
+      ++count[c];
+      while (count[c] == k)
+        --count[s.charAt(l++) - 'a'];
+      ans -= r - l + 1;
     }
+
+    return ans;
+  }
 }
