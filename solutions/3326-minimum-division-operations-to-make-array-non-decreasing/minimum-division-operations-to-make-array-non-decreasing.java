54de75dc8e8a57bd0b5ab51956c3339b7e01f908
Varun Sharma <9204varun@gmail.com>
2024-10-26 01:55:11 +0530

    Time: 603 ms (23.36%) | Memory: 57.9 MB (71.11%) - LeetSync

--- solutions/3326-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.java
+++ solutions/3326-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.java
@@ -1,23 +1,26 @@
 class Solution {
-  public int minOperations(int[] nums) {
-    int ans = 0;
-
-    for (int i = nums.length - 2; i >= 0; --i)
-      if (nums[i] > nums[i + 1]) {
-        final int minDivisor = getMinDivisor(nums[i]);
-        if (minDivisor > nums[i + 1])
-          return -1;
-        nums[i] = minDivisor;
-        ++ans;
-      }
-
-    return ans;
-  }
-
-  private int getMinDivisor(int num) {
-    for (int divisor = 2; divisor <= Math.sqrt(num); ++divisor)
-      if (num % divisor == 0)
-        return divisor;
-    return num;
-  }
+    int divisor(int n){
+        int div = 1;
+        for (int i=1;i<=Math.sqrt(n);i++) {
+            if (n%i==0) {
+                div = Math.max(div, i);
+                if (n/i<n) div = Math.max(div, n / i);
+            }
+        }
+        return div;
+    }
+    public int minOperations(int[] nums) {
+        int ans=0; int n = nums.length;
+        for(int i=n-2;i>=0;i--){
+            while (nums[i]>nums[i+1]) {
+                int gpd = divisor(nums[i]);
+                if (gpd==1 && nums[i] != 1) {
+                    return -1;
+                }
+                ans++;
+                nums[i] /= gpd;
+            }
+        }
+        return ans;
+    }
 }
