e32fcb8429ac2b520963a79d20d184fb229e2ac9
Varun Sharma <9204varun@gmail.com>
2024-11-05 14:24:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (89.17%) - LeetSync

--- solutions/3274-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
+++ solutions/3274-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,19 +1,11 @@
 class Solution {
-    public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
-        Map<Character, Integer> m = new HashMap<>();
+  public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
+    return squareIsWhite(coordinate1) == squareIsWhite(coordinate2);
+  }
 
-        m.put('a', 1);
-        m.put('b', 2);
-        m.put('c', 3);
-        m.put('d', 4);
-        m.put('e', 5);
-        m.put('f', 6);
-        m.put('g', 7);
-        m.put('h', 8);
-
-        int a = m.get(coordinate1.charAt(0)) + Integer.valueOf(coordinate1.charAt(1));
-        int b = m.get(coordinate2.charAt(0)) + Integer.valueOf(coordinate2.charAt(1));
-
-        return a%2 == b%2;
-    }
+  private boolean squareIsWhite(final String coordinates) {
+    final char letter = coordinates.charAt(0);
+    final char digit = coordinates.charAt(1);
+    return letter % 2 != digit % 2;
+  }
 }
