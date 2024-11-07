913aba2ef94670873e7e1d14f2e07928a04419c4
Varun Sharma <9204varun@gmail.com>
2024-11-07 22:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (49.36%) - LeetSync

--- solutions/897-increasing-order-search-tree/increasing-order-search-tree.java
+++ solutions/897-increasing-order-search-tree/increasing-order-search-tree.java
@@ -1,15 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode increasingBST(TreeNode root) {
-        return increasingBST(root, null);
+
+        return bst(root, null);
     }
 
-    private TreeNode increasingBST(TreeNode root, TreeNode tail) {
-        if (root == null)
-            return tail;
 
-        TreeNode ans = increasingBST(root.left, root);
+    public TreeNode bst(TreeNode root, TreeNode temp){
+        if(root == null){
+            return temp;
+        }
+
+        TreeNode tail = bst(root.left, root);
         root.left = null;
-        root.right = increasingBST(root.right, tail);
-        return ans;
+        root.right = bst(root.right, temp);
+
+        return tail;
     }
 }
+
