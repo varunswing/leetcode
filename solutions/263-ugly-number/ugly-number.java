d4991eb62a79f54bc4e00e6755c22c42159c73cf
Varun Sharma <9204varun@gmail.com>
2024-11-07 22:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (16.81%) - LeetSync

---
+++ solutions/263-ugly-number/ugly-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n==0){
+            return false;
+        }
+        
+
+        for(int a : new int[]{2, 3, 5}){
+            while(n%a == 0){
+                n /= a;
+            }
+        }
+
+        return n == 1;
+    }
+}
