0a59f70609fd1f34eed955ae7785480a998473ac
Varun Sharma <9204varun@gmail.com>
2024-11-08 15:10:41 +0530

    Time: 16 ms (29.17%) | Memory: 46.2 MB (11.55%) - LeetSync

--- solutions/888-fair-candy-swap/fair-candy-swap.java
+++ solutions/888-fair-candy-swap/fair-candy-swap.java
@@ -1,37 +1,14 @@
 class Solution {
-    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
-        int ta = 0, tb = 0, total = 0;
+  public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+    final int diff = (Arrays.stream(aliceSizes).sum() - Arrays.stream(bobSizes).sum()) / 2;
+    Set<Integer> bobSizesSet = Arrays.stream(bobSizes).boxed().collect(Collectors.toSet());
 
-        Set<Integer> sa = new TreeSet<>();
-        Set<Integer> sb =  new TreeSet<>();
-
-        for(int i=0; i<aliceSizes.length; i++){
-            ta += aliceSizes[i];
-            sa.add(aliceSizes[i]);
-        }
-        for(int i=0; i<bobSizes.length; i++){
-            tb += bobSizes[i];
-            sb.add(bobSizes[i]);
-        }
-        int[] ans = new int[2];
-        total = (ta+tb)/2;
-
-        int i = 0, j=0;
-        while(aliceSizes.length > j || bobSizes.length > i){
-            if(ta > tb){
-                if(sa.contains(total - tb + bobSizes[i])){
-                    return new int[]{total - tb + bobSizes[i], bobSizes[i]};
-                }
-                i++;
-            }else{
-                if(sb.contains(total - ta + aliceSizes[j])){
-                    return new int[]{aliceSizes[j], total - ta + aliceSizes[j++]};
-                }
-                j++;
-            }
-        }
-
-
-        return ans;
+    for (final int aliceSize : aliceSizes) {
+      final int target = aliceSize - diff;
+      if (bobSizesSet.contains(target))
+        return new int[] {aliceSize, target};
     }
+
+    throw new IllegalArgumentException();
+  }
 }
