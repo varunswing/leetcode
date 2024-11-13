7f49068ff316b350a6c03e61ace5c58c9c146dae
Varun Sharma <9204varun@gmail.com>
2024-11-14 00:56:41 +0530

    Time: 17 ms (37.39%) | Memory: 45.1 MB (35.20%) - LeetSync

--- solutions/929-unique-email-addresses/unique-email-addresses.java
+++ solutions/929-unique-email-addresses/unique-email-addresses.java
@@ -1,36 +1,13 @@
 class Solution {
-    public int numUniqueEmails(String[] emails) {
-        Set<String> ans = new HashSet<>();
-        for(String s : emails){
-            StringBuilder sb = new StringBuilder();
-            boolean flag = true;
-            boolean isDomain = false;
-            for(int i=0; i<s.length(); i++){
-                if(!isDomain){
-                    if(s.charAt(i) == '.' && !isDomain){
-                        continue;
-                    }else if(s.charAt(i) == '+'){
-                        flag = false;
-                    }else if(!flag && s.charAt(i) == '@'){
-                        flag = true;
-                        isDomain = true;
-                        sb.append(s.charAt(i));
-                    }else if(s.charAt(i) == '@'){
-                        isDomain = true;
-                        sb.append(s.charAt(i));
-                    }else if(flag){
-                        sb.append(s.charAt(i));
-                    }
-                }else{
-                    sb.append(s.charAt(i));
-                }
-                
-            }
-            ans.add(sb.toString());
-        }
-        for(String s : ans){
-            System.out.println(s);
-        }
-        return ans.size();
+  public int numUniqueEmails(String[] emails) {
+    Set<String> normalized = new HashSet<>();
+
+    for (final String email : emails) {
+      String[] parts = email.split("@");
+      String[] local = parts[0].split("\\+");
+      normalized.add(local[0].replace(".", "") + "@" + parts[1]);
     }
+
+    return normalized.size();
+  }
 }
