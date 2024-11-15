179a4f9347141606ceb469a7a6ba43df26e8a236
Varun Sharma <9204varun@gmail.com>
2024-11-15 17:33:04 +0530

    Time: 3 ms (93.91%) | Memory: 43.2 MB (69.43%) - LeetSync

--- solutions/2696-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ solutions/2696-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,27 +1,19 @@
 class Solution {
-    public int minLength(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        for(int i=0; i<sb.length()-1; i++){
-            if(sb.charAt(i) == 'A'){
-                if(i+1 < sb.length() && sb.charAt(i+1) == 'B'){
-                    sb.delete(i, i+2);
-                    i-=2;
-                    if(i<0){
-                        i = -1;
-                    }
-                    continue;
-                }
-            }
-            if(sb.charAt(i) == 'C'){
-                if(i+1 < sb.length() && sb.charAt(i+1) == 'D'){
-                    sb.delete(i, i+2);
-                    i -= 2;
-                    if(i<0){
-                        i = -1;
-                    }
-                }
-            }
-        }
-        return sb.length();
-    }
+  public int minLength(String s) {
+    Deque<Character> stack = new ArrayDeque<>();
+
+    for (final char c : s.toCharArray())
+      if (c == 'B' && match(stack, 'A'))
+        stack.pop();
+      else if (c == 'D' && match(stack, 'C'))
+        stack.pop();
+      else
+        stack.push(c);
+
+    return stack.size();
+  }
+
+  private boolean match(Deque<Character> stack, char c) {
+    return !stack.isEmpty() && stack.peek() == c;
+  }
 }
