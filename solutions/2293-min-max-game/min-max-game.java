42ccc414770e7d2ae04792ccb58c1a98ffb0ff70
Varun Sharma <9204varun@gmail.com>
2024-11-19 20:06:58 +0530

    Time: 1 ms (58.90%) | Memory: 43.6 MB (14.13%) - LeetSync

--- solutions/2293-min-max-game/min-max-game.java
+++ solutions/2293-min-max-game/min-max-game.java
@@ -1,6 +1,9 @@
 class Solution {
     public int minMaxGame(int[] nums) {
         int n = nums.length;
+        if(n==1){
+            return nums[0];
+        }
 
         while (n > 1) {
             for (int i = 0; i < n / 2; i++) {
