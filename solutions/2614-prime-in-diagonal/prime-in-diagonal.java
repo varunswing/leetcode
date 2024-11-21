b6384919b20664946a3568a457b32d2c5f3abd80
Varun Sharma <9204varun@gmail.com>
2024-11-21 17:49:37 +0530

    Time: 5 ms (79.80%) | Memory: 59.3 MB (51.63%) - LeetSync

--- solutions/2614-prime-in-diagonal/prime-in-diagonal.java
+++ solutions/2614-prime-in-diagonal/prime-in-diagonal.java
@@ -1,29 +1,23 @@
 class Solution {
-    public int diagonalPrime(int[][] nums) {
-        List<Integer> dia = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-            dia.add(nums[i][i]);
-            dia.add(nums[i][nums.length - 1 - i]);
-        }
-
-        Collections.sort(dia);
-        int ans = 0;
-
-
-        for(int i=dia.size()-1; i>=0; i--){
-            boolean flag = false;
-            int a = dia.get(i);
-            for(int j=(int) Math.sqrt(a); j > 1; j--){
-                if(a%j == 0){
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag && a != 1){
-                return a;
-            }
-        }
-
-        return ans;
+  public int diagonalPrime(int[][] nums) {
+    int ans = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      final int a = nums[i][i];
+      final int b = nums[i][nums.length - i - 1];
+      if (isPrime(a))
+        ans = Math.max(ans, a);
+      if (isPrime(b))
+        ans = Math.max(ans, b);
     }
+    return ans;
+  }
+
+  private boolean isPrime(int n) {
+    if (n <= 1)
+      return false;
+    for (int i = 2; i * i <= n; ++i)
+      if (n % i == 0)
+        return false;
+    return true;
+  }
 }
