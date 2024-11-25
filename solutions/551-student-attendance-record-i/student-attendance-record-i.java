80adc0bf0dfbc3600785b57c6299cf5b4d9dd980
Varun Sharma <9204varun@gmail.com>
2024-11-25 23:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (7.32%) - LeetSync

--- solutions/551-student-attendance-record-i/student-attendance-record-i.java
+++ solutions/551-student-attendance-record-i/student-attendance-record-i.java
@@ -1,16 +1,5 @@
 class Solution {
     public boolean checkRecord(String s) {
-        int count = 0;
-
-        for(char c : s.toCharArray()){
-            if(c == 'A'){
-                count++;
-                if(count > 1){
-                    return false;
-                }
-            }
-        }
-
-        return s.contains("LLL") == false;
+        return s.indexOf('A') == s.lastIndexOf('A') && !s.contains("LLL");
     }
 }
