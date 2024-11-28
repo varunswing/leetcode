c068778900fd00e0efc5469c3d11f6987094a7cc
Varun Sharma <9204varun@gmail.com>
2024-11-28 16:14:22 +0530

    Time: 1 ms (33.33%) | Memory: 41.8 MB (5.71%) - LeetSync

--- solutions/1085-sum-of-digits-in-the-minimum-number/sum-of-digits-in-the-minimum-number.java
+++ solutions/1085-sum-of-digits-in-the-minimum-number/sum-of-digits-in-the-minimum-number.java
@@ -1,21 +1,13 @@
 class Solution {
     public int sumOfDigits(int[] nums) {
-        int mn = nums[0];
-        for(int num : nums){
-            mn = Math.min(num, mn);
-        }
+        int mn = Arrays.stream(nums).min().getAsInt();
 
         int ans = 0;
         while(mn > 0){
-            int a = mn%10;
+            ans += mn%10;
             mn /=10;
-            ans+=a;
         }
 
-        if((ans&1) != 0){
-            return 0;
-        }
-
-        return 1;
+        return ans&1^1;
     }
 }
