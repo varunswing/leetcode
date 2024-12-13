4ecdef771134339c52021f57e4399150aa37d38d
Varun Sharma <9204varun@gmail.com>
2024-12-13 20:46:08 +0530

    Time: 4 ms (31.73%) | Memory: 44.8 MB (38.98%) - LeetSync

--- solutions/1213-intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.java
+++ solutions/1213-intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.java
@@ -1,23 +1,26 @@
 class Solution {
     public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
-        int n = Math.max(Math.max(arr1[arr1.length - 1], arr2[arr2.length - 1]), arr3[arr3.length-1]);
-
         List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        int k = 0;
 
-        int[] arr = new int[n+1];
-
-        for(int i=0; i<arr1.length; i++){
-            arr[arr1[i]]++;
-        }
-        for(int i=0; i<arr2.length; i++){
-            arr[arr2[i]]++;
-        }
-        for(int i=0; i<arr3.length; i++){
-            arr[arr3[i]]++;
-            if(arr[arr3[i]] == 3){
-                ans.add(arr3[i]);
+        while (i < arr1.length && j < arr2.length && k < arr3.length) {
+            final int mn = Math.min(arr1[i], Math.min(arr2[j], arr3[k]));
+            if (arr1[i] == mn && arr2[j] == mn && arr3[k] == mn) {
+                ans.add(mn);
+                ++i;
+                ++j;
+                ++k;
+            } else if (arr1[i] == mn) {
+                ++i;
+            } else if (arr2[j] == mn) {
+                ++j;
+            } else {
+                ++k;
             }
         }
+
         return ans;
     }
 }
