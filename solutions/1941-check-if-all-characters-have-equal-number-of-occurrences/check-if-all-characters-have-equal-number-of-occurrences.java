3c5ab9e3af72af417a00c3d47cd13ec5564ad443
Varun Sharma <9204varun@gmail.com>
2024-12-15 15:54:05 +0530

    Time: 6 ms (66.94%) | Memory: 41.8 MB (82.60%) - LeetSync

--- solutions/1941-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
+++ solutions/1941-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -6,17 +6,6 @@ class Solution {
             freq[s.charAt(i)-'a']++;
         }
 
-        int ans = 0;
-        for(int i=0; i<26; i++){
-            if(freq[i] != 0 && ans == 0){
-                ans = freq[i];
-            }
-            if(freq[i] != 0 && freq[i] != ans){
-                return false;
-            }
-            
-        }
-
-        return true;
+        return Arrays.stream(freq).allMatch(f -> f == 0 || f == freq[s.charAt(0)-'a']);
     }
 }
