5b6298a65917f52d72eeca65e29deaf16421943c
Varun Sharma <9204varun@gmail.com>
2025-03-08 18:32:32 +0530

    Time: 1 ms (92.10%) | Memory: 41.8 MB (26.70%) - LeetSync

--- solutions/2379-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ solutions/2379-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,38 +1,16 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int n = blocks.length();
+        int countB = 0;
+        int maxCountB = 0;
 
-        int[] bc = new int[n];
-        int[] wc = new int[n];
-        int w = 0;
-        int b = 0;
-
-        for(int i=0; i<n; i++){
-            if(blocks.charAt(i) == 'W'){
-                w++;
-            }else{
-                b++;
-            }
-            wc[i] = w;
-            bc[i] = b;
+        for (int i = 0; i < blocks.length(); ++i) {
+            if (blocks.charAt(i) == 'B')
+                ++countB;
+            if (i >= k && blocks.charAt(i - k) == 'B')
+                --countB;
+            maxCountB = Math.max(maxCountB, countB);
         }
 
-        int ans = k;
-
-        for(int i=k-1; i<n; i++){
-            int t;
-            if(i < k){
-                t = k - bc[i];
-            }else{
-                t = k - bc[i] + bc[i-k];
-            }
-            ans = Math.min(ans, t);
-        }
-
-        if(n == k){
-            return k - bc[n-1];
-        }
-
-        return ans;
+        return k - maxCountB;
     }
 }
